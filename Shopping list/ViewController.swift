7f435d7be4ddc7ad9befda1b09666a8641b78c8c
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-24 17:47:21 +0300

    Initial Commit

---
+++ Shopping list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping list
+//
+//  Created by Ekaterina Akchurina on 24.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
